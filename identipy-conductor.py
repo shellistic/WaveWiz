9505f6d53f38ecabd5c39c480fc69036846fd76e
Morty <36650362+mortycode@users.noreply.github.com>
2018-06-24 07:57:36 -0500

    Minor clean-up; removed unnecessary comments related to testing, and converted all single-quoted strings to uniform double-quoted strings.

--- identipy-conductor.py
+++ identipy-conductor.py
@@ -1,8 +1,3 @@
-# Project to implement an OOP approach - ! DO NOT USE !
-#
-# [0] = εᵣ = u"\u03B5\u1D63" = "Relative Permittivity"
-#  [1] = σ = u"\u03C3" = "Conductivity Constant"
-#   [2]= μᵣ = u"\u03BC\u1D63" = "Relative Permeability"
 from math import pi, sqrt
 
 
@@ -33,27 +28,27 @@ class MaterialDict(dict):
 
 
 preloaded_dict = MaterialDict({
-    'air': (1, 0, 1.00000037),
-    'fresh water': (80, 5e-4, 0.999992),
-    'sea water': (80, 3, 1),
-    'ice': (3.5, 1e-5, 1),
-    'clay': (20, 5, 1),
-    'saturated sand': (30, 1e-4, 1),
-    'barium titanate': (3279, 1e-6, 1),
-    'cold rolled steel': (1, 1e-7, 100),
-    'purified iron': (1, 1e-7, 5e3),
-    'mu metal': (1, 1.6e6, 2e5),
-    '2-81 permalloy': (1, 1e2, 1e6),
-    'copper': (1, 5.96e7, 0.999994),
-    'gold': (1, 4.1e7, 1),
-    'aluminium': (1, 3.5e7, 1.000022),
-    'tungsten': (1, 1.79e7, 1),
-    'graphite': (12.5, 2e5, 1),
-    'diamond': (7.5, 1e-13, 0.99999975),
-    'silicon': (11.68, 1.56e-3, 1),
-    'glass': (65, 1e-15, 1),
-    'kiln dried wood': (4, 1e-15, 1.000000435),
-    'ptfe': (2.1, 1e-25, 1)
+    "air": (1, 0, 1.00000037),
+    "fresh water": (80, 5e-4, 0.999992),
+    "sea water": (80, 3, 1),
+    "ice": (3.5, 1e-5, 1),
+    "clay": (20, 5, 1),
+    "saturated sand": (30, 1e-4, 1),
+    "barium titanate": (3279, 1e-6, 1),
+    "cold rolled steel": (1, 1e-7, 100),
+    "purified iron": (1, 1e-7, 5e3),
+    "mu metal": (1, 1.6e6, 2e5),
+    "2-81 permalloy": (1, 1e2, 1e6),
+    "copper": (1, 5.96e7, 0.999994),
+    "gold": (1, 4.1e7, 1),
+    "aluminium": (1, 3.5e7, 1.000022),
+    "tungsten": (1, 1.79e7, 1),
+    "graphite": (12.5, 2e5, 1),
+    "diamond": (7.5, 1e-13, 0.99999975),
+    "silicon": (11.68, 1.56e-3, 1),
+    "glass": (65, 1e-15, 1),
+    "kiln dried wood": (4, 1e-15, 1.000000435),
+    "ptfe": (2.1, 1e-25, 1)
 })
 
 
@@ -147,20 +142,20 @@ def main_funct(mat):
 
 
 while True:
-    main_prompt = (input('\nWelcome to the Conductor Calculator!\n'
-                         'A dictionary of sample materials at sample values '
-                         'to test out has been pre-loaded. To see them, type '
-                         '\'help\'.\n'
-                         'To test your own custom material and values, enter '
-                         '\'add\' to add it to the dictionary of materials.\n'
-                         'To test a material, ensure it is available in the '
-                         'dictionary and type its name in the prompt below.\n'
-                         'What material are you working with? ').lower())
-    if main_prompt == 'help':
-        print('\nCurrently contains the following materials:\n\n')
+    main_prompt = (input("\nWelcome to the Conductor Calculator!\n\n"
+                         "A dictionary of sample materials at sample values "
+                         "to test out has been pre-loaded. To see them, type "
+                         "'help'.\n\n"
+                         "To test your own custom material and values, enter "
+                         "'add' to add it to the dictionary of materials.\n\n"
+                         "To try a material, ensure it is available in the "
+                         "dictionary and type its name in the prompt below.\n"
+                         "What material are you working with? ").lower())
+    if main_prompt == "help":
+        print("\nCurrently contains the following materials:\n\n")
         print(preloaded_dict.keys())
-        print('Type one of the provided material names when prompted.\n')
-    elif main_prompt == 'add':
+        print("Type one of the provided material names when prompted.\n")
+    elif main_prompt == "add":
         prompt1 = input("Would you like to add a material "
                         "to the current material dictionary?\n"
                         "[Y]es or [N]o ").lower()
@@ -174,8 +169,8 @@ while True:
                                                   "permittivity "
                                                   "(\u03B5\u1D63) value? "))
                     except ValueError:
-                        print(f'That is not a valid value...\n'
-                              'Please use only numbers for this value.')
+                        print(f"That is not a valid value...\n"
+                              "Please use only numbers for this value.")
                     else:
                         break
                 while True:
@@ -183,8 +178,8 @@ while True:
                         mat_cconst = float(input("What is its conductivity "
                                                  "constant (\u03C3) value? "))
                     except ValueError:
-                        print(f'That is not a valid value...\n'
-                              'Please use only numbers for this value.')
+                        print(f"That is not a valid value...\n"
+                              "Please use only numbers for this value.")
                     else:
                         break
                 while True:
@@ -193,8 +188,8 @@ while True:
                                                   "permeability "
                                                   "(\u03BC\u1D63) value? "))
                     except ValueError:
-                        print(f'That is not a valid value...\n'
-                              'Please use only numbers for this value.')
+                        print(f"That is not a valid value...\n"
+                              "Please use only numbers for this value.")
                     else:
                         break
                 is_valid = input("You are trying to add:\n"
@@ -203,7 +198,7 @@ while True:
                                  f"\u03C3 of {mat_cconst},\n"
                                  f"and a \u03BC\u1D63 of {mat_rpermea}\n"
                                  "Is this correct ([Y]es or [N]o)?\n").lower()
-                if is_valid[0] == 'y':
+                if is_valid[0] == "y":
                     print("Added your new material to the dictionary!")
                     preloaded_dict.update(
                         {mat_name: (mat_rpermit, mat_cconst, mat_rpermea)}
@@ -213,25 +208,25 @@ while True:
         try:
             main_funct(preloaded_dict[main_prompt])
             while True:
-                again_prompt = input('Would you like to try another'
-                                     ' calculation?\n'
-                                     'Please type either'
-                                     ' Yes or No:\n\n').lower()
-                if again_prompt == '':
-                    print('No input given. Please type either Yes or No:\n\n')
-                elif again_prompt[0] == 'n':
-                    print('\nGoodbye!\n\n\n\n')
+                again_prompt = input("Would you like to try another "
+                                     "calculation?\n"
+                                     "Please type either "
+                                     "Yes or No:\n\n").lower()
+                if again_prompt == "":
+                    print("No input given. Please type either Yes or No:\n\n")
+                elif again_prompt[0] == "n":
+                    print("\nGoodbye!\n\n\n\n")
                     quit()
-                elif again_prompt[0] == 'y':
+                elif again_prompt[0] == "y":
                     break
                 else:
-                    print('\nI didn\'t quite understand that...')
+                    print("\nI didn't quite understand that...")
         except KeyError:
             if main_prompt == '':
-                print('\n You did not specify a material.\n'
-                      ' Please use a valid material listed in the'
-                      ' \'help\' command.\n')
+                print("\nYou did not specify a material.\n"
+                      "Please use a valid material listed in the "
+                      "'help' command.\n")
             else:
-                print(f'\n {main_prompt} is not a valid material.\n'
-                      ' Please use a valid material listed in the'
-                      ' \'help\' command.\n')
+                print(f"\n{main_prompt} is not a valid material or command.\n"
+                      "Please use a valid material listed in the "
+                      "'help' command.\n")
