b39e82b4317a9b025ecdfdc541ef434bec8bc0fb
Morty <36650362+mortycode@users.noreply.github.com>
2018-07-19 20:43:14 -0500

    Bug Fixes and Cleanup:
    Removed unnecessary string interpolation from print function on lines 176, 185, 195
    
    Changed __repr__ method on class MaterialDict to more accurately 'represent' MaterialDicts to Python
    
    Added __str__ method to 'represent' MaterialDicts to others
    
    FIXED: Issue #1: Users can enter blank material names by entering a NULL value using the update() method
    
    FIXED: Issue #2: If an empty string is given when asked 'Is this correct [Y]es or [N]o?', Python will throw "IndexError: string index out of range".
    
    FIXED: Issue #3: If an empty string is given when asked 'Would you like to add a material...' using the 'add' update() method, Python will throw "IndexError: string index out of range".
    
    Added a 'quit' option to gracefully quit from the main entry loop.
    
    Clarified responses if blank input given to 'main_prompt'
    
    Clarified main welcome banner and instructions, and cleaned it up by separating the banner text from the input prompt text in the script.
    
    Cleanup of extra spaces/whitespace.

--- pyconductor.py
+++ pyconductor.py
@@ -7,6 +7,9 @@ class MaterialDict(dict):
         super().__init__(*arg, **kw)
 
     def __repr__(self):
+        return "MaterialDict object"
+
+    def __str__(self):
         if len(self) == 0:
             return "empty material dictionary"
         else:
@@ -25,7 +28,6 @@ class MaterialDict(dict):
 
     def count(self):
         return len(self)
-
 
 preloaded_dict = MaterialDict({
     "air": (1, 0, 1.00000037),
@@ -51,7 +53,6 @@ preloaded_dict = MaterialDict({
     "ptfe": (2.1, 1e-25, 1)
 })
 
-
 def main_funct(mat):
     """
     Main Function [main_funct()] takes the material provided in 'main_prompt'
@@ -62,10 +63,10 @@ def main_funct(mat):
     """
     while True:
         try:
-            freq = float(input('\nWhat frequency (in Hertz) is the material'
-                               ' operating at? '))
+            freq = float(input("\nWhat frequency (in Hertz) is "
+                               "the material operating at? "))
         except ValueError:
-            print('Please type in only numbers for operating frequency...')
+            print("Please type in only numbers for operating frequency...")
         else:
             break
 
@@ -74,137 +75,152 @@ def main_funct(mat):
     w = 2 * pi * freq * mat[0] * eo
     test = mat[1] / w
     if test == 0:
-        print(f'\n  While operating at {freq}Hz, {main_prompt}'
-              f' acts as a Lowless Medium!')
         alpha = 0
-        print('\n  The attenuation constant, alpha, '
-              f'has a value of {round(alpha, 3)} Np/m, and ')
         beta = (w / (sqrt(uo * mat[2] * eo * mat[0])))
-        print(f'  beta has a value of {round(beta, 3)} rad/m.')
         nc = ((mat[2] * uo) / (eo * mat[0]))
-        print('  The intrinsic impedance of this lowless'
-              f' medium is {round(nc, 3)} ohms.')
         up = (1 / (mat[0] * eo * mat[2] * uo))
         lam = float(up / freq)
-        print(f'\n  The phase velocity is {round(up, 3)} meters per second\n'
-              f'  with a wavelength of {round(lam, 3)} meters.\n')
+        print(
+            f"\n  While operating at {freq}Hz, {main_prompt} acts as a "
+            "Lowless Medium!\n\n  The attenuation constant, alpha, has a "
+            f"value of {round(alpha, 3)} Np/m, and beta has a value of "
+            f"{round(beta, 3)} rad/m.\n  The intrinsic impedance of this "
+            f"lowless medium is {round(nc, 3)} ohms.\n  The phase velocity is "
+            f"{round(up, 3)} meters per second with a wavelength of "
+            f"{round(lam, 3)} meters.\n")
         input('Press Enter to continue... \n')
     elif test <= 0.01:
-        print(f'\n  While operating at {freq}Hz, {main_prompt} '
-              f'acts as a Low-Less Medium!')
         alpha = ((mat[1] / 2) * sqrt((uo * mat[2])/(eo * mat[0])))
-        print('\n  The attenuation constant, alpha, '
-              f'has a value of {round(alpha, 3)} Np/m, and ')
         beta = (w / (sqrt(uo * mat[2] * eo * mat[0])))
-        print(f'  beta has a value of {round(beta, 3)} rad/m.')
         nc = ((mat[2] * uo) / (eo * mat[0]))
-        print('  The intrinsic impedance of this low-less '
-              f'medium is {round(nc, 3)} ohms.')
         up = (1 / (mat[0] * eo * mat[2] * uo))
         lam = float(up / freq)
-        print(f'\n  The phase velocity is {round(up, 3)} meters per second\n'
-              f'  with a wavelength of {round(lam, 3)} meters.\n')
+        print(
+            f"\n  While operating at {freq}Hz, {main_prompt} acts as a "
+            "Low-Less Medium!\n\n  The attenuation constant, alpha, has a "
+            f"value of {round(alpha, 3)} Np/m, and beta has a value of "
+            f"{round(beta, 3)} rad/m.\n  The intrinsic impedance of this "
+            f"low-less medium is {round(nc, 3)} ohms.\n  The phase velocity "
+            f"is {round(up, 3)} meters per second with a wavelength of "
+            f"{round(lam, 3)} meters.\n")
         input('Press Enter to continue... \n')
     elif test >= 100:
-        print(f'\n  While operating at {freq}Hz, {main_prompt}'
-              f' acts as a Good Conductor!')
         alpha = sqrt(pi * freq * uo * mat[2] * mat[1])
-        print('\n  The attenuation constant, alpha, '
-              f'has a value of {round(alpha, 3)} Np/m, and ')
         beta = sqrt(pi * freq * uo * mat[2] * mat[1])
-        print(f'  beta has a value of {round(beta, 3)} rad/m.')
         nc = complex((1 + 1j) * (alpha / mat[1]))
-        print('  The intrinsic impedance of this '
-              f'good conductor is {nc} ohms.')
         up = sqrt(4 * pi * freq * uo * mat[2] * mat[1])
         lam = up / freq
-        print(f'\n  The phase velocity is {round(up, 3)} meters per second\n'
-              f'  with a wavelength of {round(lam, 3)} meters.\n')
+        print(
+            f"\n  While operating at {freq}Hz, {main_prompt} acts as a "
+            "Good Conductor!\n\n  The attenuation constant, alpha, has a "
+            f"value of {round(alpha, 3)} Np/m, and beta has a value of "
+            f"{round(beta, 3)} rad/m.\n  The intrinsic impedance of this "
+            f"good conductor is {nc} ohms.\n  The phase velocity is "
+            f"{round(up, 3)} meters per second with a wavelength of "
+            f"{round(lam, 3)} meters.\n")
         input('Press Enter to continue...\n')
     else:
-        print(f'\n  While operating at {freq}Hz, {main_prompt}'
-              f' acts as an Any Medium!')
         alpha = (w * (sqrt((uo * mat[2] * eo * mat[0]) *
                            sqrt(1 + (test ** 2)) - 1)))
-        print('\n  The attenuation constant, alpha, '
-              f'has a value of {round(alpha, 3)} Np/m, and ')
         beta = (w * (sqrt((uo * mat[2] * eo * mat[0]) *
                           sqrt(1 + (test ** 2)) + 1)))
-        print(f'  beta has a value of {round(beta, 3)} rad/m.')
         nc = complex((sqrt((uo * mat[2]) / (eo * mat[0]))) *
                      sqrt((1 - (1j * test))))
-        print('  The intrinsic impedance of this any medium is '
-              f'{nc} ohms.')
         up = (w / beta)
         lam = ((2 * pi) / beta)
-        print(f'\n  The phase velocity is {round(up, 3)} meters per second\n'
-              f'  with a wavelength of {round(lam, 3)} meters.\n')
+        print(
+            f"\n  While operating at {freq}Hz, {main_prompt} acts as an "
+            "Any Medium!\n  The attenuation constant, alpha, has a value "
+            f"of {round(alpha, 3)} Np/m,\n  and beta has a value of "
+            f"{round(beta, 3)} rad/m.\n  The intrinsic impedance of this "
+            f"any medium is {nc} ohms.\n  The phase velocity is "
+            f"{round(up, 3)} meters per second\n  with a wavelength of "
+            f"{round(lam, 3)} meters.\n")
         input('Press Enter to continue...\n')
 
-
 while True:
-    main_prompt = (input("\nWelcome to the Conductor Calculator!\n\n"
-                         "A dictionary of sample materials at sample values "
-                         "to test out has been pre-loaded. To see them, type "
-                         "'help'.\n\n"
-                         "To test your own custom material and values, enter "
-                         "'add' to add it to the dictionary of materials.\n\n"
-                         "To try a material, ensure it is available in the "
-                         "dictionary and type its name in the prompt below.\n"
-                         "What material are you working with? ").lower())
+    print(
+        "\nWelcome to the Conductor Calculator!\n\n"
+        "A dictionary of sample materials at sample values to test out has "
+        "been pre-loaded. To see it, type 'help'.\nThis will also list any "
+        "custom materials that you have added to the dictionary using 'add'; "
+        "they will be added to the end of the dictionary.\n\nTo test your own "
+        "custom material and values, enter 'add' to add it to the dictionary "
+        "of materials.\n\nTo try a material, ensure it is available in the "
+        "dictionary and type its name in the prompt below.\n\n"
+        "To exit, simply type 'quit'.\n")
+    main_prompt = input("Please input a material name or a command: ").lower()
     if main_prompt == "help":
         print("\nCurrently contains the following materials:\n\n")
         print(preloaded_dict.keys())
         print("Type one of the provided material names when prompted.\n")
     elif main_prompt == "add":
-        prompt1 = input("Would you like to add a material "
-                        "to the current material dictionary?\n"
-                        "[Y]es or [N]o ").lower()
-
-        if prompt1[0] == "y":
-            while True:
-                mat_name = input("OK!\nWhat is your material named? ").lower()
+        while True:
+            prompt1 = input("Would you like to add a material "
+                            "to the current material dictionary?\n"
+                            "[Y]es or [N]o ").lower()
+            if prompt1 in ("y", "yes"):
                 while True:
-                    try:
-                        mat_rpermit = float(input("What is its relative "
-                                                  "permittivity "
-                                                  "(\u03B5\u1D63) value? "))
-                    except ValueError:
-                        print(f"That is not a valid value...\n"
-                              "Please use only numbers for this value.")
-                    else:
+                    while True:
+                        mat_name = input("OK!\nWhat is your material named? ").lower()
+                        if mat_name == "":
+                            print("\nPlease type in a material name\n")
+                        else:
+                            break
+                    while True:
+                        try:
+                            mat_rpermit = float(input("What is its relative "
+                                                    "permittivity "
+                                                    "(\u03B5\u1D63) value? "))
+                        except ValueError:
+                            print("That is not a valid value...\n"
+                                "Please use only numbers for this value")
+                        else:
+                            break
+                    while True:
+                        try:
+                            mat_cconst = float(input("What is its conductivity "
+                                                    "constant (\u03C3) value? "))
+                        except ValueError:
+                            print("That is not a valid value...\n"
+                                "Please use only numbers for this value")
+                        else:
+                            break
+                    while True:
+                        try:
+                            mat_rpermea = float(input("What is its relative "
+                                                    "permeability "
+                                                    "(\u03BC\u1D63) value? "))
+                        except ValueError:
+                            print("That is not a valid value...\n"
+                                "Please use only numbers for this value")
+                        else:
+                            break
+                    while True:
+                        is_valid = input("You are trying to add:\n"
+                                        f"{mat_name}\n"
+                                        f"with a \u03B5\u1D63 of {mat_rpermit},\n"
+                                        f"\u03C3 of {mat_cconst},\n"
+                                        f"and a \u03BC\u1D63 of {mat_rpermea}\n"
+                                        "Is this correct ([Y]es or [N]o)?\n").lower()
+                        if is_valid == "":
+                            print("Please enter yes or no, or simply y/n")
+                        else:
+                            break
+                    if is_valid in ("y", "yes"):
+                        print("\nAdded your new material to the dictionary!\n\n")
+                        preloaded_dict.update(
+                            {mat_name: (mat_rpermit, mat_cconst, mat_rpermea)}
+                        )
                         break
-                while True:
-                    try:
-                        mat_cconst = float(input("What is its conductivity "
-                                                 "constant (\u03C3) value? "))
-                    except ValueError:
-                        print(f"That is not a valid value...\n"
-                              "Please use only numbers for this value.")
-                    else:
-                        break
-                while True:
-                    try:
-                        mat_rpermea = float(input("What is its relative "
-                                                  "permeability "
-                                                  "(\u03BC\u1D63) value? "))
-                    except ValueError:
-                        print(f"That is not a valid value...\n"
-                              "Please use only numbers for this value.")
-                    else:
-                        break
-                is_valid = input("You are trying to add:\n"
-                                 f"{mat_name}\n"
-                                 f"with a \u03B5\u1D63 of {mat_rpermit},\n"
-                                 f"\u03C3 of {mat_cconst},\n"
-                                 f"and a \u03BC\u1D63 of {mat_rpermea}\n"
-                                 "Is this correct ([Y]es or [N]o)?\n").lower()
-                if is_valid[0] == "y":
-                    print("\nAdded your new material to the dictionary!\n\n")
-                    preloaded_dict.update(
-                        {mat_name: (mat_rpermit, mat_cconst, mat_rpermea)}
-                    )
-                    break
+                break
+            elif prompt1 in ("n", "no"):
+                break
+            else:
+                print("\nPlease enter yes or no, or simply y/n\n")
+    elif main_prompt == "quit":
+        print("\nGoodbye!\n\n\n\n")
+        quit()
     else:
         try:
             main_funct(preloaded_dict[main_prompt])
@@ -214,7 +230,7 @@ while True:
                                      "Please type either "
                                      "Yes or No:\n\n").lower()
                 if again_prompt == "":
-                    print("No input given. Please type either Yes or No:\n\n")
+                    print("No input given. Please type either Yes or No\n\n")
                 elif again_prompt[0] == "n":
                     print("\nGoodbye!\n\n\n\n")
                     quit()
@@ -223,11 +239,11 @@ while True:
                 else:
                     print("\nI didn't quite understand that...")
         except KeyError:
-            if main_prompt == '':
-                print("\nYou did not specify a material.\n"
+            if main_prompt == "":
+                print("\nYou did not specify a material or command.\n"
                       "Please use a valid material listed in the "
-                      "'help' command.\n")
+                      "'help' command, or use 'add' to add your own.\n")
             else:
                 print(f"\n{main_prompt} is not a valid material or command.\n"
                       "Please use a valid material listed in the "
-                      "'help' command.\n")
+                      "'help' command, or use 'add' to add your own.\n")
