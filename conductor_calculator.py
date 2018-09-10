77867d6c4ee30650023f3ec2a8318edd92530264
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-09-09 20:19:06 -0500

    refactor conductor_calculator & playing with FTs; still need practice

--- conductor_calculator.py
+++ conductor_calculator.py
@@ -1,38 +1,42 @@
 from pyconductor import load_test_values, calculate_conductance
 
-preloaded_dict = load_test_values()
 
-while preloaded_dict:
-    print(
-        "[1] - Show currently available materials in Material Dictionary\n"
-        "[2] - Add a material (will not be saved upon restart)\n"
-        "[3] - Quit program\n"
-        "To test the conductive properties of a material, simply type in its name.\n"
-        "Otherwise, type the corresponding number for an option above.\n"
-    )
-    main_prompt = input(">>> ").lower()
-    if main_prompt == "1":
-        print(f"\nCurrently contains the following materials:\n{preloaded_dict.keys()}\n")
-    elif main_prompt == "2":
-        preloaded_dict.addmat()
-    elif main_prompt == "3":
-        quit()
-    else:
-        try:
-            calculate_conductance(preloaded_dict[main_prompt])
-            while True:
-                again_prompt = input(
-                    "Would you like to try another calculation? [Y]es or [N]o: ").lower()
-                if again_prompt in ("y", "yes"):
-                    break
-                elif again_prompt in ("n", "no"):
-                    print("\nGoodbye!\n")
-                    quit()
-        except KeyError:
-            if main_prompt == "":
-                print("\nNo material specified.\nPlease enter a valid material name "
-                      "listed in option [1], or use option [2] to add your own.\n")
-            else:   # TODO: add logic handling whether user wants to add missing material
-                print(f"\n{main_prompt} is not a valid material or command!\n")
+def conductance_calc():
+    preloaded_dict = load_test_values()
+    while preloaded_dict:
+        print(
+            "[1] - Show currently available materials in Material Dictionary\n"
+            "[2] - Add a material (will not be saved upon restart)\n"
+            "[3] - Quit\n"
+            "To test the conductive properties of a material, simply type in its name.\n"
+            "Otherwise, type the corresponding number for an option above.\n"
+        )
+        main_prompt = input(">>> ").lower()
+        if main_prompt == "1":
+            print(f"\nCurrently contains the following materials:\n{preloaded_dict.keys()}\n")
+        elif main_prompt == "2":
+            preloaded_dict.addmat()
+        elif main_prompt == "3":
+            quit()
         else:
-            pass
+            try:
+                calculate_conductance(preloaded_dict[main_prompt])
+                while True:
+                    again_prompt = input(
+                        "Would you like to try another calculation? [Y]es or [N]o: ").lower()
+                    if again_prompt in ("y", "yes"):
+                        break
+                    elif again_prompt in ("n", "no"):
+                        print("\nGoodbye!\n")
+                        quit()
+            except KeyError:
+                if main_prompt == "":
+                    print("\nNo material specified.\nPlease enter a valid material name "
+                          "listed in option [1], or use option [2] to add your own.\n")
+                else:   # TODO: add logic handling whether user wants to add missing material
+                    print(f"\n{main_prompt} is not a valid material or command!\n")
+            else:
+                pass
+
+if __name__ == "__main__":
+    conductance_calc()
--- functional_tests.py
+++ functional_tests.py
@@ -1,15 +1,18 @@
 import unittest
 
-
+from pyconductor import *
 
 
 class NewUserTest(unittest.TestCase):
 
-    def test_user_can_view_current_materials_in_materialdict(self):
-        pass
+    def setUp(self):
+        self.preloaded_dict = load_test_values()
 
     def test_user_can_run_material_testcase(self):
-        pass
+        calculate_conductance(self.preloaded_dict["air"])
 
     def test_user_can_add_material_to_materialdict(self):
         pass
+
+if __name__ == "__main__":
+    unittest.main()
